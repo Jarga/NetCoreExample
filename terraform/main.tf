6da6f9e55f5f76bc9f137c4f3e3247932780fab8
Sean McAdams <sean.j.mcadams@gmail.com>
2017-06-30 15:42:19 -0400

    Add terraform shells

---
+++ terraform/main.tf
@@ -0,0 +1,333 @@
+# Configure the Azure Providers
+
+terraform {
+  backend "azure" {
+    storage_account_name = "smcadamsterraformstate"
+    container_name       = "smcadamsterraformstate"
+    key                  = "prod.terraform.tfstate"
+	  resource_group_name  = "Default-Storage-EastUS"
+	  arm_subscription_id  = ""
+	  arm_client_id        = ""
+	  arm_client_secret    = ""
+	  arm_tenant_id        = ""
+  }
+}
+
+provider "azurerm" {
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group}"
+  location = "${var.location}"
+}
+
+resource "azurerm_availability_set" "avset" {
+  name                         = "${var.dns_name}avset"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
+}
+
+resource "azurerm_public_ip" "lbpip" {
+  name                         = "${var.rg_prefix}-ip"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.rg.name}"
+  public_ip_address_allocation = "Static"
+  domain_name_label            = "${var.lb_ip_dns_name}"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.virtual_network_name}"
+  location            = "${var.location}"
+  address_space       = ["${var.address_space}"]
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.rg_prefix}subnet"
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  address_prefix       = "${var.subnet_prefix}"
+}
+
+resource "azurerm_lb" "lb" {
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  name                = "${var.rg_prefix}lb"
+  location            = "${var.location}"
+
+  frontend_ip_configuration {
+    name                 = "LoadBalancerFrontEnd"
+    public_ip_address_id = "${azurerm_public_ip.lbpip.id}"
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id     = "${azurerm_lb.lb.id}"
+  name                = "BackendPool1"
+}
+
+resource "azurerm_lb_nat_rule" "tcp" {
+  resource_group_name            = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id                = "${azurerm_lb.lb.id}"
+  name                           = "SSH-VM-${count.index}"
+  protocol                       = "tcp"
+  frontend_port                  = "2${count.index + 21}"
+  backend_port                   = 22
+  frontend_ip_configuration_name = "LoadBalancerFrontEnd"
+  count                          = "${var.vm_count}"
+}
+
+resource "azurerm_lb_rule" "lb_rule" {
+  count                          = "${length(var.lb_ports)}"
+  resource_group_name            = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id                = "${azurerm_lb.lb.id}"
+  name                           = "LBRule-${count.index}"
+  protocol                       = "tcp"
+  frontend_port                  = "${element(var.lb_ports, count.index)}"
+  backend_port                   = "${element(var.lb_ports, count.index)}"
+  frontend_ip_configuration_name = "LoadBalancerFrontEnd"
+  enable_floating_ip             = false
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.backend_pool.id}"
+  idle_timeout_in_minutes        = 5
+  probe_id                       = "${element(azurerm_lb_probe.lb_probe.*.id, count.index)}"
+  depends_on                     = ["azurerm_lb_probe.lb_probe"]
+}
+
+resource "azurerm_lb_probe" "lb_probe" {
+  count               = "${length(var.lb_ports)}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  loadbalancer_id     = "${azurerm_lb.lb.id}"
+  name                = "tcpProbe-${count.index}"
+  protocol            = "tcp"
+  port                = "${element(var.lb_ports, count.index)}"
+  interval_in_seconds = 15
+  number_of_probes    = 2
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic${count.index}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  count               = "${var.vm_count}"
+
+  ip_configuration {
+    name                                    = "ipconfig${count.index}"
+    subnet_id                               = "${azurerm_subnet.subnet.id}"
+    private_ip_address_allocation           = "Dynamic"
+    load_balancer_backend_address_pools_ids = ["${azurerm_lb_backend_address_pool.backend_pool.id}"]
+    load_balancer_inbound_nat_rules_ids     = ["${element(azurerm_lb_nat_rule.tcp.*.id, count.index)}"]
+  }
+}
+
+resource "tls_private_key" "sshkey" {
+  algorithm   = "RSA"
+}
+
+resource "azurerm_storage_account" "stor" {
+  name                = "${var.dns_name}stor"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  account_type        = "${var.storage_account_type}"
+}
+
+resource "azurerm_storage_share" "share" {
+  name = "swarmshare"
+
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+  storage_account_name = "${azurerm_storage_account.stor.name}"
+
+  quota = 1
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm${count.index}"
+  location              = "${var.location}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  availability_set_id   = "${azurerm_availability_set.avset.id}"
+  vm_size               = "${var.vm_size}"
+  network_interface_ids = ["${element(azurerm_network_interface.nic.*.id, count.index)}"]
+  delete_os_disk_on_termination = true
+  count                 = "${var.vm_count}"
+
+  storage_image_reference {
+    publisher = "${var.image_publisher}"
+    offer     = "${var.image_offer}"
+    sku       = "${var.image_sku}"
+    version   = "${var.image_version}"
+  }
+
+  storage_os_disk {
+    name          = "osdisk${count.index}"
+    caching       = "ReadWrite"
+    create_option = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "${var.hostname}${count.index}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
+  }
+  
+  os_profile_linux_config {
+    disable_password_authentication = false
+	ssh_keys = { 
+      path = "/home/${var.admin_username}/.ssh/authorized_keys"
+	  key_data = "${tls_private_key.sshkey.public_key_openssh}"
+    }
+  }
+}
+
+resource "null_resource" "mount-shares" {
+  count = "${var.vm_count}"
+  depends_on = ["azurerm_virtual_machine.vm"]
+  
+  triggers {
+    key = "${uuid()}"
+  }
+
+  connection {
+      host = "${azurerm_public_ip.lbpip.fqdn}"
+      user = "${var.admin_username}"
+	  private_key = "${tls_private_key.sshkey.private_key_pem}"
+	  port = "2${count.index + 21}"
+      type = "ssh"
+      timeout = "4m"
+	  agent = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt --fix-broken install",
+      "sudo apt-get update",
+      "sudo apt-get -f -y install",
+      "sudo apt-get -y upgrade",
+      "sudo apt-get install cifs-utils",
+	  "sudo mkdir /mnt/swarmdata",
+	  "sudo mount -t cifs ${replace(azurerm_storage_share.share.url, "https:", "")} /mnt/swarmdata -o vers=3.0,user=${var.dns_name}stor,password=${azurerm_storage_account.stor.primary_access_key},dir_mode=0777,file_mode=0777,serverino"
+    ]
+  }
+}
+
+resource "null_resource" "configure-swarm-manager" {
+  count = 1
+  depends_on = ["null_resource.mount-shares"]
+  
+  triggers {
+    key = "${uuid()}"
+  }
+
+  connection {
+      host = "${azurerm_public_ip.lbpip.fqdn}"
+      user = "${var.admin_username}"
+	  private_key = "${tls_private_key.sshkey.private_key_pem}"
+	  port = "2${count.index + 21}"
+      type = "ssh"
+      timeout = "4m"
+	  agent = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "command -v docker >/dev/null 2>&1 || sudo curl -sSL https://get.docker.com/ | sh",
+	  "sudo usermod -aG docker ${var.admin_username}",
+	  "echo sudo docker swarm init --advertise-addr ${element(azurerm_network_interface.nic.*.private_ip_address, count.index)}",
+	  "sudo docker swarm init --advertise-addr ${element(azurerm_network_interface.nic.*.private_ip_address, count.index)} || true",
+	  "sudo docker swarm join-token -q worker > /mnt/swarmdata/workertoken",
+	  "sudo docker swarm join-token -q manager > /mnt/swarmdata/managertoken"
+    ]
+  }
+}
+
+resource "null_resource" "configure-add-swarm-managers" {
+  count = 2
+  depends_on = ["null_resource.configure-swarm-manager"]
+  
+  triggers {
+    key = "${uuid()}"
+  }
+  
+  connection {
+      host = "${azurerm_public_ip.lbpip.fqdn}"
+      user = "${var.admin_username}"
+	  private_key = "${tls_private_key.sshkey.private_key_pem}"
+	  port = "2${count.index + 22}"
+      type = "ssh"
+      timeout = "4m"
+	  agent = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "command -v docker >/dev/null 2>&1 || sudo curl -sSL https://get.docker.com/ | sh",
+	  "echo sudo docker swarm join --token $(cat /mnt/swarmdata/managertoken) ${azurerm_network_interface.nic.0.private_ip_address}",
+	  "sudo docker swarm join --token $(cat /mnt/swarmdata/managertoken) ${azurerm_network_interface.nic.0.private_ip_address} || true"
+    ]
+  }
+}
+
+resource "null_resource" "configure-swarm-agents" {
+  count = "${var.vm_count - 3}"
+  depends_on = ["null_resource.configure-swarm-manager"]
+  
+  triggers {
+    key = "${uuid()}"
+  }
+  
+  connection {
+      host = "${azurerm_public_ip.lbpip.fqdn}"
+      user = "${var.admin_username}"
+	  private_key = "${tls_private_key.sshkey.private_key_pem}"
+	  port = "2${count.index + 24}"
+      type = "ssh"
+      timeout = "4m"
+	  agent = false
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "command -v docker >/dev/null 2>&1 || sudo curl -sSL https://get.docker.com/ | sh",
+	  "echo sudo docker swarm join --token $(cat /mnt/swarmdata/workertoken) ${azurerm_network_interface.nic.0.private_ip_address}",
+	  "sudo docker swarm join --token $(cat /mnt/swarmdata/workertoken) ${azurerm_network_interface.nic.0.private_ip_address} || true"
+    ]
+  }
+}
+
+resource "null_resource" "run-system-containers" {
+  count = 1
+  depends_on = ["null_resource.configure-swarm-manager"]
+  
+  triggers {
+    key = "${uuid()}"
+  }
+  
+  connection {
+      host = "${azurerm_public_ip.lbpip.fqdn}"
+      user = "${var.admin_username}"
+	    private_key = "${tls_private_key.sshkey.private_key_pem}"
+	    port = "2${count.index + 21}"
+      type = "ssh"
+      timeout = "4m"
+	    agent = false
+  }
+
+  # Copies the myapp.conf file to /etc/myapp.conf
+  provisioner "file" {
+    source      = "grafana-compose.yml"
+    destination = "/tmp/grafana-compose.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo docker service create --name=viz --publish=8080:8080/tcp --constraint=node.role==manager --mount=type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock dockersamples/visualizer || true",
+	    "sudo docker service create --name portainer --publish 9000:9000 --constraint 'node.role == manager' --mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock portainer/portainer -H unix:///var/run/docker.sock || true",
+      "docker stack deploy -c /tmp/grafana-compose.yml monitor || true"
+    ]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,90 @@
+variable "resource_group" {
+  description = "The name of the resource group in which to create the virtual network."
+  default     = "New-Web-EastUS"
+}
+
+variable "rg_prefix" {
+  description = "The shortened abbreviation to represent your resource group that will go on the front of some resources."
+  default     = "rg"
+}
+
+variable "hostname" {
+  description = "VM name referenced also in storage-related names."
+}
+
+variable "dns_name" {
+  description = " Label for the Domain Name. Will be used to make up the FQDN. If a domain name label is specified, an A DNS record is created for the public IP in the Microsoft Azure DNS system."
+}
+
+variable "lb_ip_dns_name" {
+  description = "DNS for Load Balancer IP"
+}
+
+variable "location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  default     = "eastus"
+}
+
+variable "virtual_network_name" {
+  description = "The name for the virtual network."
+  default     = "vnet"
+}
+
+variable "address_space" {
+  description = "The address space that is used by the virtual network. You can supply more than one address space. Changing this forces a new resource to be created."
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_prefix" {
+  description = "The address prefix to use for the subnet."
+  default     = "10.0.10.0/24"
+}
+
+variable "storage_account_type" {
+  description = "Defines the type of storage account to be created. Valid options are Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS. Changing this is sometimes valid - see the Azure documentation for more information on which types of accounts can be converted into other types."
+  default     = "Standard_LRS"
+}
+
+variable "vm_size" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_A1"
+}
+
+variable "vm_count" {
+  description = "number of VMs to create"
+  default = 5
+}
+
+variable "image_publisher" {
+  description = "name of the publisher of the image (az vm image list)"
+  default     = "Canonical"
+}
+
+variable "image_offer" {
+  description = "the name of the offer (az vm image list)"
+  default     = "UbuntuServer"
+}
+
+variable "image_sku" {
+  description = "image sku to apply (az vm image list)"
+  default     = "16.04-LTS"
+}
+
+variable "image_version" {
+  description = "version of the image to apply (az vm image list)"
+  default     = "latest"
+}
+
+variable "admin_username" {
+  description = "administrator user name"
+}
+
+variable "admin_password" {
+  description = "administrator password (recommended to disable password auth)"
+}
+
+variable "lb_ports" {
+  description = "Ports to expose in Load Balancer"
+  type = "list"
+  default = [80, 3000, 8080, 9000, 15672, 27017]
+}
